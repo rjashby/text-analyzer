c791b842248030374dcd7f0a84c73eda83908e43
Ryan Ashby <rjapsu160@gmail.com>
2021-10-26 12:48:05 -0700

    add new function noInputtedWord as utility function, and DRY/refactor code.
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
+  }
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
@@ -17,7 +24,7 @@ function wordCounter(text) {
 // wordCounter() function omitted for brevity.
 
 function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -57,6 +64,7 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
 
