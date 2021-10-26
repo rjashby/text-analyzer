6d978e8321df9b2f5ab6f211b133d2dbceb9bbc3
Ryan Ashby <rjapsu160@gmail.com>
2021-10-26 11:17:26 -0700

    add 3rd function to business logic, with corresponding tests in README.
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,22 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+function boldPassage(word, text) {
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
 // UI Logic
 
 $(document).ready(function(){
@@ -43,3 +59,5 @@ $(document).ready(function(){
     $("#selected-count").html(occurrencesOfWord);
   });
 });
+
+
